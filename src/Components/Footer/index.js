837671abd9eb56ebf8930bd3d6e3e4c4eb4db096
Agastinmellimi <125130023+Agastinmellimi@users.noreply.github.com>
2024-02-17 19:10:31 +0530

    updated text

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -30,7 +30,7 @@ const Footer = () => {
                     <FooterText>Thank you for visiting the <span style={{color: "#fff"}}>LJCPC</span> website, if you want to contact prayer request's and other details please click on Contact Us section and send your Message.</FooterText>
                     <Btn type='button' onClick={navigateContact}>Contact Us</Btn>
                     </FooterConetentContainer>
-                    <CreatorName>Designed by <span style={{color: "#78e6f5"}}><span style={{color: "#e6e47e"}}>@</span>Agastin Mellimi.</span></CreatorName>
+                    <CreatorName>designed by <span style={{color: "#78e6f5"}}><span style={{color: "#e6e47e"}}>@</span>Agastin Mellimi.</span></CreatorName>
                 </FooterContainer>
             )
         }}
